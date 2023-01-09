3d03d2ee04a5a5f1874ad8c18b2ad252d933da6e
Michael <michalluczak4a@gmail.com>
2023-01-09 19:54:46 +0100

    Add reference to JavaPlugin to Command

--- src/main/java/me/taison/AbstractCommand.java
+++ src/main/java/me/taison/AbstractCommand.java
@@ -41,7 +41,7 @@ public abstract class AbstractCommand {
         return argumentsMap;
     }
 
-    public JavaPlugin getJavaPlugin() {
+    public JavaPlugin getPlugin() {
         return javaPlugin;
     }
 
